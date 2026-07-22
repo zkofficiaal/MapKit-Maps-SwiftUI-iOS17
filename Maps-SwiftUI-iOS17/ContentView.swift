3be4ccc88a08f09b597f1c6ea1ced8f78078c916
Muhammad Zahid Khan <zkofficiaal@gmail.com>
2026-07-22 00:29:04 -0700

    first commit

--- Maps-SwiftUI-iOS17/ContentView.swift
+++ Maps-SwiftUI-iOS17/ContentView.swift
@@ -6,16 +6,11 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Map()
     }
 }
 
