89dc16ebe4448fd2f8eb6aca2e1a13a69eb91786
Muhammad Zahid Khan <zkofficiaal@gmail.com>
2026-07-22 03:47:18 -0700

    Add single marker for Apple Visitor Centre in SwiftUI Map

--- Maps-SwiftUI-iOS17/ContentView.swift
+++ Maps-SwiftUI-iOS17/ContentView.swift
@@ -9,12 +9,19 @@ import SwiftUI
 import MapKit
 
 struct ContentView: View {
-    let cameraPosition : MapCameraPosition = .region(.init(center: .init(latitude: 37.3346, longitude: -122.0099), latitudinalMeters: 1300, longitudinalMeters: 1300))
+    let cameraPosition : MapCameraPosition = .region(.init(center: .init(latitude: 37.3346, longitude: -122.0099), latitudinalMeters: 13000, longitudinalMeters: 13000))
     var body: some View {
-        Map(initialPosition: cameraPosition)
+        Map(initialPosition: cameraPosition){
+            Marker("Apple visitor centre",systemImage: "laptopcomputer", coordinate: .appleVisitorCentre)
+        }
     }
 }
 
 #Preview {
     ContentView()
+}
+
+extension CLLocationCoordinate2D {
+    static let applleHQ = CLLocationCoordinate2D(latitude: 37.3346, longitude: -122.0099)
+    static let appleVisitorCentre = CLLocationCoordinate2D(latitude: 37.332753, longitude: -122.005372)
 }
