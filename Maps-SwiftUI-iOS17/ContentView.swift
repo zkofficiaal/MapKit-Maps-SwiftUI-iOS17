caa93ae0f0755bd9d0be2e5664ab6c42e5c63c88
Muhammad Zahid Khan <zkofficiaal@gmail.com>
2026-07-22 04:24:17 -0700

    Add custom SwiftUI Annotation

--- Maps-SwiftUI-iOS17/ContentView.swift
+++ Maps-SwiftUI-iOS17/ContentView.swift
@@ -9,10 +9,24 @@ import SwiftUI
 import MapKit
 
 struct ContentView: View {
-    let cameraPosition : MapCameraPosition = .region(.init(center: .init(latitude: 37.3346, longitude: -122.0099), latitudinalMeters: 13000, longitudinalMeters: 13000))
+    let cameraPosition : MapCameraPosition = .region(.init(center: .init(latitude: 37.3346, longitude: -122.0099), latitudinalMeters: 1300, longitudinalMeters: 1300))
     var body: some View {
         Map(initialPosition: cameraPosition){
-            Marker("Apple visitor centre",systemImage: "laptopcomputer", coordinate: .appleVisitorCentre)
+//            Marker("Apple visitor centre",systemImage: "laptopcomputer", coordinate: .appleVisitorCentre)
+//            Marker("panamaPark", systemImage: "tree.fil", coordinate: .panamaPark)
+//                .tint(.green)
+            
+            Annotation("AppleHQ", coordinate: .applleHQ, anchor: .bottom){
+                Image(systemName: "laptopcomputer")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .foregroundStyle(.white)
+                    .frame(width: 20, height: 20)
+                    .padding()
+                    .background(.pink.gradient,in: .circle)
+            }
+            
+            
         }
     }
 }
@@ -24,4 +38,5 @@ struct ContentView: View {
 extension CLLocationCoordinate2D {
     static let applleHQ = CLLocationCoordinate2D(latitude: 37.3346, longitude: -122.0099)
     static let appleVisitorCentre = CLLocationCoordinate2D(latitude: 37.332753, longitude: -122.005372)
+    static let panamaPark = CLLocationCoordinate2D(latitude: 37.347745, longitude: -122.018715)
 }
